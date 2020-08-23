899946da12820917927b4636f218863000892917
MiguelRocha <miguelrocha@ua.pt>
2020-08-23 11:34:18 +0100

    Added sample unit test to zimfilechecks

--- test/zimcheck-test.cpp
+++ test/zimcheck-test.cpp
@@ -2,17 +2,21 @@
 
 #include "zim/zim.h"
 #include "zim/file.h"
+#include "../src/zimfilechecks.h"
 
 
-TEST(Zimcheck, getFileSize)
+TEST(Zimcheck, overalStatus)
 {
   std::string fn = "data/zimfiles/wikibooks_be_all_nopic_2017-02.zim";
 
 //    ASSERT_TRUE(fileExists(fn));
 
     zim::File file(fn);
+    ErrorLogger logger;
     
+    test_checksum(file, logger);
 
+    ASSERT_TRUE(logger.overalStatus());
     
 
 }
